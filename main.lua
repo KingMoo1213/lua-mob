90db2513d11c9304232ac383594efce80d5f266d
KingMoo1213 <cag.atkinson@gmail.com>
2023-04-25 18:26:53 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 function love.load()
   love.window.setTitle("Lua Test")
-  love.graphics.setBackgroundColor(192, 192, 192) -- silver background
-  love.graphics.setColor(255, 215, 0) -- golden circle
-  -- love.graphics.setCircleSegments(100) -- smooth circle
+  love.graphics.setBackgroundColor(192/255, 192/255, 192/255) -- silver background
 end
 
 function love.draw()
+  love.graphics.setColor(255/255, 215/255, 0) -- golden circle
   love.graphics.circle("fill", love.graphics.getWidth()/2, love.graphics.getHeight()/2, 100) -- golden circle at center of screen
-  love.graphics.setColor(64, 64, 64) -- dark grey text
+  love.graphics.setColor(64/255, 64/255, 64/255) -- dark grey text
   love.graphics.print("This is a Lua test", 10, 10) -- dark grey text at top-left corner
 end
