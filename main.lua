4e1a4dbddc07068c369f6d3c6e6dc4778cdc3b55
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 07:52:40 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,11 +18,14 @@ function love.load()
   -- local display = require("display")
   -- display.setStatusBar(display.HiddenStatusBar)
   love.graphics.setBackgroundColor(192/255, 192/255, 192/255) -- silver background
+
+    PageManager:switchTo("splash") 
 end
 
 function love.draw()
-  love.graphics.setColor(255/255, 215/255, 0) -- golden circle
-  love.graphics.circle("fill", love.graphics.getWidth()/2, love.graphics.getHeight()/2, 100) -- golden circle at center of screen
-  love.graphics.setColor(64/255, 64/255, 64/255) -- dark grey text
-  love.graphics.print("This is a Lua test", 10, 10) -- dark grey text at top-left corner
+  --love.graphics.setColor(255/255, 215/255, 0) -- golden circle
+  --love.graphics.circle("fill", love.graphics.getWidth()/2, love.graphics.getHeight()/2, 100) -- golden circle at center of screen
+  --love.graphics.setColor(64/255, 64/255, 64/255) -- dark grey text
+  --love.graphics.print("This is a Lua test", 10, 10) -- dark grey text at top-left corner
+    PageManager:render()
 end
