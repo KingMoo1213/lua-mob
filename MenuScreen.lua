81ef4c7b89fd22f8a8c056456b6261c1aeb92de4
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 14:09:33 +0000

    Update MenuScreen.lua

--- MenuScreen.lua
+++ MenuScreen.lua
@@ -4,11 +4,15 @@ require "InputManager"
 MenuScreen = Page:new("Menu Screen", "green")
 
 function MenuScreen:touchreleased(id, x, y, dx, dy, pressure)
-    if InputManager.touchTimer >= InputManager.touchThreshold then
-        -- Show progress bar here if needed
+    local touchInfo = InputManager:touchreleased(id, x, y, dx, dy, pressure)
+    if touchInfo.eventType == "press" then
+        -- Logic for long press
         PageManager:switchTo("game")
+    elseif touchInfo.eventType == "tap" then
+        -- Logic for tap
     end
 end
+
 
 function MenuScreen:render()
     -- Custom rendering for the menu screen
