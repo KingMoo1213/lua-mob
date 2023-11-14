ed1d21c0182d0d06a3358a31578324c413f82ca0
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 08:48:47 +0000

    Update gamescreen.lua

--- gamescreen.lua
+++ gamescreen.lua
@@ -1,6 +1,17 @@
 require "Page"
+require "InputManager"
 
 GameScreen = Page:new("Game Screen", "orange")
+
+
+function GameScreen:touchreleased(id, x, y, dx, dy, pressure)
+    if InputManager.touchTimer < InputManager.touchThreshold then
+        -- Change background color on tap
+    else
+        local direction = InputManager:getSwipeDirection()
+        -- Display swipe direction
+    end
+end
 
 function GameScreen:render()
     -- Custom rendering for the game screen
