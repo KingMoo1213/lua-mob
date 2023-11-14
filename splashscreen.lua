b6cc78bc63d0a346b315ef5bf64a0058bce4e373
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 08:47:08 +0000

    Update splashscreen.lua

--- splashscreen.lua
+++ splashscreen.lua
@@ -1,6 +1,13 @@
 require "Page"
+require "InputManager"
 
 SplashScreen = Page:new("Splash Screen", "blue")
+
+function SplashScreen:touchreleased(id, x, y, dx, dy, pressure)
+    if InputManager.touchTimer < InputManager.touchThreshold then
+        PageManager:switchTo("menu")
+    end
+end
 
 function SplashScreen:render()
     -- Custom rendering for the splash screen
