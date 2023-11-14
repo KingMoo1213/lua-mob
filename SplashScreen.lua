bb81e65a92c572325d684b65c782dcae4b3a3584
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 17:18:41 +0000

    Update SplashScreen.lua

--- SplashScreen.lua
+++ SplashScreen.lua
@@ -1,18 +1,29 @@
-require "Page"
-require "InputManager"
+local AnimationManager = require "AnimationManager"
+local ImageManager = require "ImageManager"
 
-SplashScreen = Page:new("Splash Screen", "blue")
+SplashScreen = Page:new("Splash Screen", {0.5, 0.5, 0.5})
 
-function SplashScreen:touchreleased(touchInfo)
-    if touchInfo.eventType == "tap" then
-        -- Logic for tap
+function SplashScreen:init()
+    -- Retrieve the image by its unique name
+    local image = ImageManager.get("splash_image")
+    
+    local screenWidth, screenHeight = love.graphics.getDimensions()
+    local imageX = (screenWidth - image:getWidth()) / 2
+    local imageY = screenHeight
+    local targetY = 0
+
+    self.animation = AnimationManager.createSlideAndFadeAnimation(image, 3, imageX, imageY, imageX, targetY)
+end
+
+function SplashScreen:update(dt)
+    AnimationManager.update(self.animation, dt)
+
+    if not self.animation.isActive then
         PageManager:switchTo("menu")
     end
 end
 
-
 function SplashScreen:render()
-    -- Custom rendering for the splash screen
     Page.render(self)
-    -- Additional splash screen elements
+    AnimationManager.draw(self.animation)
 end
