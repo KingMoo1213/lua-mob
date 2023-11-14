41388049017fff4f894fbebbe8be71a2fd756652
KingMoo1213 <cag.atkinson@gmail.com>
2023-11-14 22:18:23 +0000

    Update AnimationManager.lua

--- AnimationManager.lua
+++ AnimationManager.lua
@@ -1,44 +1,53 @@
 local AnimationManager = {}
 
-function AnimationManager.createSlideAndFadeAnimation(image, duration, startX, startY, endX, endY)
+function AnimationManager.createAnimation(properties)
+    -- properties include target object, property to animate, start and end values, duration, delay, loop type, etc.
     return {
-        image = image,
-        duration = duration,
-        startX = startX,
-        startY = startY,
-        endX = endX,
-        endY = endY,
-        currentTime = 0,
-        isActive = true
+        target = properties.target,
+        property = properties.property,
+        startValue = properties.startValue,
+        endValue = properties.endValue,
+        duration = properties.duration,
+        delay = properties.delay or 0,
+        elapsedTime = 0,
+        loopType = properties.loopType or "none", -- Options: "none", "loop", "yoyo"
+        direction = 1, -- 1 for forward, -1 for backward (used in yoyo)
+        isPlaying = true,
+        -- Add more properties as needed
     }
 end
 
 function AnimationManager.update(animation, dt)
-    if animation == nil then return end
-    if not animation.isActive then return end
+    if not animation.isPlaying then return end
 
-    animation.currentTime = animation.currentTime + dt
-    local progress = animation.currentTime / animation.duration
-    if progress > 1 then
-        progress = 1
+    animation.elapsedTime = animation.elapsedTime + dt
+
+    -- Handle delay
+    if animation.elapsedTime < animation.delay then return end
+
+    local progress = (animation.elapsedTime - animation.delay) / animation.duration
+    progress = math.min(progress, 1) -- Clamp progress to 1
+
+    if animation.loopType == "loop" and progress >= 1 then
+        animation.elapsedTime = animation.delay
+    elseif animation.loopType == "yoyo" and progress >= 1 then
+        animation.elapsedTime = animation.delay
+        animation.direction = -animation.direction
+    elseif progress >= 1 then
+        animation.isPlaying = false
     end
 
-    animation.currentX = animation.startX + (animation.endX - animation.startX) * progress
-    animation.currentY = animation.startY + (animation.endY - animation.startY) * progress
-    animation.alpha = 1 - progress
-
-    if animation.currentTime >= animation.duration then
-        animation.isActive = false
+    -- Calculate the interpolated value
+    local value
+    if animation.loopType == "yoyo" and animation.direction == -1 then
+        value = animation.endValue - (animation.endValue - animation.startValue) * progress
+    else
+        value = animation.startValue + (animation.endValue - animation.startValue) * progress
     end
-end
 
-function AnimationManager.draw(animation)
-    if animation == nil then return end
-    if not animation.isActive then return end
-
-    love.graphics.setColor(1, 1, 1, animation.alpha)
-    love.graphics.draw(animation.image, animation.currentX, animation.currentY)
-    love.graphics.setColor(1, 1, 1, 1) -- Resetting the color
+    -- Apply the value to the target
+    animation.target[animation.property] = value
 end
 
 return AnimationManager
+
